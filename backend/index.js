5a854623f72b5726bcd7533f51b65c1b49416b2e
Ausaf17 <kausaf98@gmail.com>
2024-10-22 17:19:47 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+//importing express
+const express = require('express');
+
+//initializing express
+const app = express();
+const port = 5000;
+
+//accept and process request
+//route
+
+// '/'
+app.get('/',(req,res)=>{
+    res.send('response from express');
+  });
+
+//add
+  app.get('/add',(req,res)=>{
+    res.send('response from add')
+  });
+  //getall
+  app.get('/getall',(req,res)=>{
+    res.send('response from getall')
+  });
+  //delete
+  app.get('/delete',(req,res)=>{
+    res.send('response from delete')
+  });
+  //update
+  app.get('/update',(req,res)=>{
+    res.send('response from updae')
+  });
+
+
+
+
+
+
+//start  the server
+app.listen(port, ()=>{
+    console.log('server started');
+    
+});
